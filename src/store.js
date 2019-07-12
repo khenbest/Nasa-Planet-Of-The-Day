0efab406819c8544602bf20adab1afe485fff923
Kenny Henbest <lekenbest@gmail.com>
2019-07-12 13:00:33 -0600

    Random Picture Working

--- src/store.js
+++ src/store.js
@@ -1,16 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import moment from 'moment'
 
 Vue.use(Vuex)
 
+const nasaApi = axios.create({
+  baseURL: 'https://api.nasa.gov/planetary',
+  timeout: 4000
+})
+
+
 export default new Vuex.Store({
   state: {
-
+    apod: {}
   },
   mutations: {
-
+    setAPOD(state, data) {
+      state.apod = data
+    }
   },
   actions: {
-
+    async getAPOD({ commit, dispatch }) {
+      let dates = [
+        "2019-01-01", "2019-01-02", "2019-01-03", "2019-01-04", "2019-01-05", "2019-01-06", "2019-01-07", "2019-01-08", "2019-01-09", "2019-01-10", "2019-01-11", "2019-01-12", "2019-01-13", "2019-01-14", "2019-01-15", "2019-01-16", "2019-01-17", "2019-01-18", "2019-01-19", "2019-01-19", "2019-01-20", "2019-01-21", "2019-01-22", "2019-01-23", "2019-01-24", "2019-01-25", "2019-01-26", "2019-01-27", "2019-01-28", "2019-01-29", "2019-01-30", "2019-01-31",
+      ]
+      let date = await dates[Math.floor(Math.random() * Math.floor(dates.length))].toString()
+      nasaApi.get('apod?api_key=T1Ldw33bcXfNyXUQunjWheAGVVKr0YuWY1CUnqCZ&date=' + date).then(res => {
+        commit("setAPOD", res.data)
+      }).catch(err => res.status(400).send(err))
+    },
+    async getAPODBySearch({ commit, dispatch }, searchDate) {
+      debugger
+      nasaApi.get('apod?api_key=T1Ldw33bcXfNyXUQunjWheAGVVKr0YuWY1CUnqCZ&date=' + searchDate).then(res => {
+        commit("setAPOD", res.data)
+      }).catch(err => res.status(400).send(err))
+    }
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,80 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container-fluid">
+    <div class="row justify-content-center vh-50">
+      <div class="form-row mb-4">
+        <div class="col-12">
+          <input v-model="searchDate" placeholder="Leave empty for random" type="date" class="inline-form ">
+          <button class="inline-form  btn btn-outline-dark" @click="manualGetPictureOfDay">Get
+            Picture!</button>
+          <div class="form-row mb-2 mt-2">
+
+            <div class="col-12">
+              <button class="inline-form form-control btn btn-outline-dark" @click="getDate()">Surprise
+                Me!</button>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-8">
+          <h1>{{pictureOfDay.title}}</h1>
+          <img :src="pictureOfDay.hdurl" class="img-fluid " alt="Picture Of The Day">
+          <h6>{{pictureOfDay.explanation}}</h6>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+  import moment from 'moment'
+
+  export default {
+    name: "pictureOfDay",
+    props: [],
+    data() {
+      return {
+        searchDate: ''
+      }
+    },
+    computed: {
+      pictureOfDay() {
+        return this.$store.state.apod
+      },
+      date() {
+        return this.$store.state.date
+      }
+    },
+    methods: {
+      manualGetPictureOfDay() {
+        this.$store.dispatch("getAPODBySearch", this.searchDate)
+      },
+      getPictureOfDay() {
+        this.$store.dispatch("getAPOD")
+      },
+      getDate() {
+        let date = new Date(new Date(2000, 0, 1).getTime() + Math.random() * (new Date().getTime() - new Date(2000, 0, 1).getTime()))
+        let randomDate = moment(date).format("YYYY-MM-DD")
+        this.$store.dispatch("getAPODBySearch", randomDate)
+
+      }
+    },
+    mounted() {
+      this.$store.dispatch("getAPOD")
+    },
+    components: {}
+  }
+</script>
+
+<style>
+  .about {
+    background-color: black;
+    opacity: 1%;
+  }
+
+  button {
+    background-color: black;
+  }
+</style>
