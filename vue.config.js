9de2cf1d4c051a3db7185bc8f580c8fcbc4e5582
Kenny Henbest <lekenbest@gmail.com>
2019-07-16 15:08:47 -0600

    try docs

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   publicPath: '/Nasa-Planet-Of-The-Day/',
-  outputDir: undefined,
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined
+  outputDir: '/docs'
 }
